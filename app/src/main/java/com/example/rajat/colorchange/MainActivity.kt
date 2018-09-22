3cd68a3945feb4ab41028f9b0c0dc893315ef1f0
therajat08 <therajat08@users.noreply.github.com>
2018-09-22 19:42:39 +0530

    Initial commit

---
+++ app/src/main/java/com/example/rajat/colorchange/MainActivity.kt
@@ -0,0 +1,42 @@
+package com.example.rajat.colorchange
+
+import android.graphics.Color
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_main.view.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+
+    fun magic(view: View){
+        val buSelected = view as Button
+
+        when(buSelected.id){
+            R.id.buBlue->{
+                color.setBackgroundColor(Color.BLUE)
+                color.setText("Blue")
+            }
+            R.id.buGreen->{
+                color.setBackgroundColor(Color.GREEN)
+                color.setText("Green")
+            }
+            R.id.buRed->{
+                color.setBackgroundColor(Color.RED)
+                color.setText("Red")
+            }
+        }
+    }
+    fun asBefore()
+    {
+        color.setText("Color appears here")
+        color.setBackgroundColor(Color.GRAY)
+    }
+}
